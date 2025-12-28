8eb9d63d64d33c92f007c6c6ff6d7e172d542639
Seanwanq <seanwang313@outlook.com>
2025-12-28 14:41:14 +0100

    Added clipboard config.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -18,3 +18,23 @@ vim.o.signcolumn = "yes"
 
 vim.cmd("syntax enable")
 vim.cmd("filetype plugin indent on")
+
+-- 设置剪贴板通用选项 (这行通常在所有平台都通用，可以放在外面)
+vim.opt.clipboard = "unnamedplus"
+
+-- 判断是否在 WSL 环境中
+if vim.fn.getenv('WSL_DISTRO_NAME') ~= vim.v.null then
+  -- WSL 专用配置：使用 win32yank.exe
+  vim.g.clipboard = {
+    name = 'win32yank-wsl',
+    copy = {
+      ['+'] = 'win32yank.exe -i --crlf',
+      ['*'] = 'win32yank.exe -i --crlf',
+    },
+    paste = {
+      ['+'] = 'win32yank.exe -o --lf',
+      ['*'] = 'win32yank.exe -o --lf',
+    },
+    cache_enabled = 0,
+  }
+end
